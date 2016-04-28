735bd114915ab0e5dced54109932889f4c4a1890
Wilson Novaes <wilsonnovaes00@hotmail.com>
2016-04-28 16:12:53 -0300

    First commit in app

--- app/index.php
+++ app/index.php
@@ -1,3 +1,18 @@
 <?php
 
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
+require_once __DIR__.'/../vendor/autoload.php';
+
+
+$app =  new Silex\Application();
+
+
+$app['debug'] = true;
+
+$app->get('/', function(){
+    return "Home";
+});
+
+$app->run();
