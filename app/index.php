0024705afef73fff59e7534ca29e005b4893413f
Wilson Novaes <wilsonnovaes00@hotmail.com>
2016-04-28 16:21:29 -0300

    Add routers and providers twig

--- app/index.php
+++ app/index.php
@@ -3,16 +3,50 @@
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
+#Add Autoload
 require_once __DIR__.'/../vendor/autoload.php';
 
 
-$app =  new Silex\Application();
+#Starting Silex Application
+$app = new Silex\Application();
 
 
+#Debugging
 $app['debug'] = true;
 
-$app->get('/', function(){
-    return "Home";
+
+#Registering a Twig Provider
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__ . '/../template',
+));
+
+
+#Routes
+$app->get('/', function () use ($app) {
+
+
+    return $app['twig']->render('home.twig');
 });
 
+
+$app->get('/contact', function () use ($app) {
+
+
+    return $app['twig']->render('contact.twig');
+});
+
+
+$app->get('/aboutme', function () use ($app) {
+
+
+    return $app['twig']->render('about.twig');
+});
+
+
+
+
+
 $app->run();
+
+
+
