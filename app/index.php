824852f0b274109eca2df16ca610cf5049ec4aa8
Wilson Novaes <wilsonnovaes00@hotmail.com>
2016-05-05 22:16:41 -0300

    Add database

--- app/index.php
+++ app/index.php
@@ -11,6 +11,13 @@ require_once __DIR__.'/../vendor/autoload.php';
 #Starting Silex Application
 $app = new Silex\Application();
 
+date_default_timezone_set('America/Sao_Paulo');
+
+#Conect Data Base
+$app['db'] = function() {
+    return new \PDO('mysql:host=localhost;dbname=silex','root','551100');
+};
+
 
 #Debugging
 $app['debug'] = true;
@@ -28,6 +35,24 @@ $app->get('/', function () use ($app) {
 
     return $app['twig']->render('home.twig');
 });
+$app->get('/listar', function() use ($app) {
+
+    $stmt = $app['db']->query("Select * from computadores");
+    $result = $stmt->fetchAll(\PDO::FETCH_ASSOC);
+
+     return $app->json($result);
+
+
+
+
+
+
+
+});
+
+
+
+
 
 $app->get('/atualizar', function () use ($app) {
 
